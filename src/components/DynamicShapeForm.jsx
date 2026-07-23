f182083ffcb0d204737b3e43b62a8c1d5439a16e
nguyenvanthien112-hub <nguyenvanthien112@gmail.com>
2026-07-23 22:21:56 +0700

    fix: support object options in DynamicShapeForm select dropdown

--- src/components/DynamicShapeForm.jsx
+++ src/components/DynamicShapeForm.jsx
@@ -56,11 +56,15 @@ function DynamicShapeForm({ shapeId, formValues, onValueChange, onGenerate }) {
                 value={formValues[field.name] || field.default}
                 onChange={(e) => onValueChange(field.name, e.target.value)}
               >
-                {field.options.map((option) => (
-                  <option key={option} value={option}>
-                    {option}
-                  </option>
-                ))}
+                {field.options.map((option) => {
+                  const val = typeof option === 'object' && option !== null ? option.value : option;
+                  const lbl = typeof option === 'object' && option !== null ? option.label : option;
+                  return (
+                    <option key={val} value={val}>
+                      {lbl}
+                    </option>
+                  );
+                })}
               </select>
             )}
 
